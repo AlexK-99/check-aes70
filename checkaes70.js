9538e7affbe932c2a4666dfe2240d1f19d93dee3
AlexK-99 <bazoo1q@gmail.com>
2024-02-08 09:37:31 +0100

    more detailed OCA function log

--- checkaes70.js
+++ checkaes70.js
@@ -1,20 +1,56 @@
 import { TCPConnection, RemoteDevice, RemoteControlClasses, Types } from 'aes70'
 import * as fs from 'fs';
 
-		TCPConnection.connect({
-            host: "10.1.11.120",
-            port: 50014,
-        })			.then((con) => {
-            let aescon = con
-            let remoteDevice = new RemoteDevice(con)
-            remoteDevice.set_keepalive_interval(1)
-            remoteDevice.get_role_map().then((map) => {
-                let keys =[ ...map.keys() ];
-                fs.writeFileSync('./msgdata.json', JSON.stringify(keys) , 'utf-8'); 
-            })
-            remoteDevice.get_device_tree().then((tree) => {
-            })
-        })
-			.catch((e) => {
-                this.log('error', 'Aes70 Device Connection Error')
-            })
+async function run()
+{
+    const connection = await TCPConnection.connect({
+        host: "10.1.11.120",
+        port: 50014,
+    });
+    const device = new RemoteDevice(connection);
+
+    device.set_keepalive_interval(1);
+
+    console.log("Device name:", await device.DeviceManager.GetModelDescription());
+
+    console.log("Object inside this device:");
+
+    const tree = await device.get_device_tree();
+
+    let allele = []
+
+    const rec = async (a) => {
+        for (let i = 0; i < a.length; i++)
+        {
+            const obj = a[i];
+
+            if (Array.isArray(obj))
+            {
+                // children
+                await rec(obj);
+            }
+      else
+      {
+          let ele = {"type":  obj.constructor.ClassName}
+          console.log("Type: %s", obj.constructor.ClassName);
+          console.log("Properties:");
+          const properties = obj.GetPropertySync();
+
+          // fetch the values of all properties from the device.
+          await properties.sync();
+
+          properties.forEach((value, name) => {
+              ele[name] = value;
+          });
+          allele.push(ele);
+          // unsubscribe all event handlers
+          properties.Dispose();
+      }
+        }
+    };
+
+    await rec(tree);
+    fs.writeFileSync('./msgdata.json', JSON.stringify(allele) , 'utf-8');
+}
+
+run().then(() => console.log("Done."));
